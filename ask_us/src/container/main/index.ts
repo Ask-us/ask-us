e4211e3f430ac69f84da09c7b0816c085eb935cd
jjunu <junukim.dev@gmail.com>
2019-11-16 21:45:39 +0900

    [UPDATE] component with custom hooks that return redux data

--- ask_us/src/container/main/index.ts
+++ ask_us/src/container/main/index.ts
@@ -1,3 +1,30 @@
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import Main from "components/main";
+import { googleLogin } from "actions/auth";
+import { AppState } from "data/store";
+
+interface SelectorType {
+  loginStatus: number;
+}
+
+export const useMainRedux = () => {
+  const dispatch = useDispatch();
+
+  const authReducerState = useSelector<AppState, SelectorType>(state => ({
+    loginStatus: state.auth.loginStatus
+  }));
+
+  const googleLoginAction = useCallback(() => {
+    dispatch(googleLogin());
+  },                                    [dispatch]);
+
+  const authReducerDispatch = {
+    googleLoginAction
+  };
+
+  return { authReducerState, authReducerDispatch };
+};
 
 export default Main;
