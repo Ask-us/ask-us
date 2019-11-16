a042ac8d30e34eaf93bc56a313cddf39b0fe4ae3
jjunu <junukim.dev@gmail.com>
2019-11-16 22:44:37 +0900

    [UPDATE] merging units

--- ask_us/src/components/main/index.tsx
+++ ask_us/src/components/main/index.tsx
@@ -2,13 +2,17 @@ import React, { FC } from "react";
 
 import * as S from "./style";
 import { SafeLine } from "components/common";
-import ButtonContainer from "./googleLogin";
+import ButtonContainer from "./buttonContainer";
+import IntroductoryPhrase from "./introductoryPhrase";
+import CreateBy from "./createBy";
 
 const Main: FC = () => {
   return (
     <S.Wrapper>
       <SafeLine>
+        <IntroductoryPhrase />
         <ButtonContainer />
+        <CreateBy />
       </SafeLine>
     </S.Wrapper>
   );
