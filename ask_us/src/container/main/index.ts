7e932498126f61747cab1ccd03f09b60fb0a814f
jjunu <junukim.dev@gmail.com>
2019-11-17 13:10:58 +0900

    [UPDATE] add reset status dispatch on dispatcher

--- ask_us/src/container/main/index.ts
+++ ask_us/src/container/main/index.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Main from "components/main";
-import { googleLogin } from "actions/auth";
+import { googleLogin, resetStatus } from "actions/auth";
 import { AppState } from "data/store";
 
 interface SelectorType {
@@ -10,18 +10,21 @@ interface SelectorType {
 }
 
 export const useMainRedux = () => {
-  const dispatch = useDispatch();
-
   const authReducerState = useSelector<AppState, SelectorType>(state => ({
     loginStatus: state.auth.loginStatus
   }));
 
+  const dispatch = useDispatch();
   const googleLoginAction = useCallback(() => {
     dispatch(googleLogin());
   },                                    [dispatch]);
+  const resetStatusAction = useCallback(() => {
+    dispatch(resetStatus());
+  },                                    [dispatch]);
 
   const authReducerDispatch = {
-    googleLoginAction
+    googleLoginAction,
+    resetStatusAction
   };
 
   return { authReducerState, authReducerDispatch };
