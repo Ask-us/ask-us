55fdda0489817afd4f8804263f861e6f6af12994
jjunu <junukim.dev@gmail.com>
2019-11-17 13:11:14 +0900

    [UPDATE] connecting button to route

--- ask_us/src/components/main/index.tsx
+++ ask_us/src/components/main/index.tsx
@@ -1,12 +1,30 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import * as S from "./style";
 import { SafeLine } from "components/common";
 import ButtonContainer from "./buttonContainer";
 import IntroductoryPhrase from "./introductoryPhrase";
 import CreateBy from "./createBy";
+import { useMainRedux } from "container/main";
+import { checkStatus } from "utils/checkers";
+import { STATUS_CODE } from "middleware/api";
 
 const Main: FC = () => {
+  const { push } = useHistory();
+  const {
+    authReducerState: { loginStatus },
+    authReducerDispatch: { resetStatusAction }
+  } = useMainRedux();
+
+  useEffect(() => {
+    if (checkStatus(loginStatus, STATUS_CODE.success)) {
+      push("/create");
+      resetStatusAction();
+      return;
+    }
+  },        [loginStatus]);
+
   return (
     <S.Wrapper>
       <SafeLine>
